eede57d8976a7ad96ab78ce46727bc55bc0ddee1
rogermparent <rogermparent@gmail.com>
2020-07-19 15:43:57 -0400

    Initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,68 @@
+const fetch = require("node-fetch");
+const { name } = require("./package.json");
+
+const reporters = (reporter) => ({
+  optionsOrPanic: (options) => {},
+});
+
+const pkgPrefix = (message) => `${name}: ${message}`;
+
+exports.onPostBuild = async function (api, options) {
+  const { reporter } = api;
+  const {
+    condition,
+    token,
+    zoneId,
+    headers = {},
+    body = { purge_everything: true },
+  } = options;
+  const { requiredOptionPanic } = reporters(reporter);
+
+  if (typeof condition === "function" && !condition(api, options)) {
+    reporter.info("Skipping due to failed condition");
+  }
+
+  const missingOptions = Object.entries({
+    token,
+    zoneId,
+  }).reduce((acc, [k, v]) => (v ? [...acc, k] : acc), []);
+
+  if (missingOptions.length > 0) {
+    reporter.panic(
+      "Some required options are missing: " + missingOptions.join(", ")
+    );
+  }
+
+  // Now that everything's initialized, we can send the request.
+
+  const res = await fetch(
+    `https://api.cloudflare.com/client/v4/zones/${zoneId}/purge_cache`,
+    {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "content-type": "application/json",
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  const body = await res.json();
+
+  if (!res.ok || !body.success) {
+    reporter.warn(
+      "response from CloudFlare indicates cache clear failure: " +
+        JSON.stringify(
+          {
+            status: res.status,
+            errors: body.errors,
+          },
+          undefined,
+          2
+        )
+    );
+  }
+
+  reporter.info("Cleared CloudFlare cache successfully");
+};
